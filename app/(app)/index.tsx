3c11f0ddc305a8e83f9b79e15b3b9d290aba02bf
Erik Velf <velf.erik@protonmail.com>
2025-01-02 21:04:31 +0100

    Modified index page for testing logout

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,26 +1,32 @@
 import { View, Text } from "react-native";
 import CustomLink from "../../shared/customLink/customLink";
-import { useAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 import { profileAtom } from "../../entities/user/model/user.state";
 import axios from "axios";
 import { API } from "../../entities/auth/api/api";
 import { useEffect } from "react";
 import { AuthResponse } from "../../entities/auth/model/auth.interfaces";
-import { loginAtom } from "../../entities/auth/model/auth.state";
+import { loginAtom, logoutAtom } from "../../entities/auth/model/auth.state";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function MyCoursesPage() {
   const [profile, setProfile] = useAtom(profileAtom);
-  // useAtom hook returns a value an an setValue like in useState
+  // useAtom hook returns a 'value' and a 'setValue' like in useState
   const [auth, login] = useAtom(loginAtom);
+  // Getting only the setter function from the atom because it's read-only
+  const logout = useSetAtom(logoutAtom);
 
   useEffect(() => {
-    console.log("Sending login details");
-    login({ email: "test@test.com", password: "password1234" });
+    // Veryfying that logout() resets our access token
+    logout();
+    AsyncStorage.getItem("auth").then((data) => {
+      console.log(data);
+    });
   }, []);
 
   return (
     <View>
-      <Text>Auth token: {auth?.accessToken}</Text>
+      <Text>Index</Text>
       <CustomLink href="/login" title="Login" />
     </View>
   );
