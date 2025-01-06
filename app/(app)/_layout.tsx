00bbec9fb96725f40e79f9ccc35a20e2196cf40a
Erik Velf <velf.erik@protonmail.com>
2025-01-06 18:35:07 +0100

    Downloading dependencies and configured menu-icon in Drawer

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,9 @@
 import { Redirect, Stack } from "expo-router";
 import { useAtomValue } from "jotai";
 import { authAtom } from "../../entities/auth/model/auth.state";
+import { Drawer } from "expo-router/drawer";
+import { Colors, Fonts } from "../../shared/tokens";
+import MenuIcon from "../../assets/icons/menu";
 
 export default function AppLayout() {
   // Promp user to login if he isn't (without rendering the rest of the page)
@@ -10,8 +13,31 @@ export default function AppLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Drawer
+      screenOptions={({ navigation }) => ({
+        headerStyle: {
+          backgroundColor: Colors.blackLight,
+        },
+        headerLeft: () => {
+          return <MenuIcon />;
+        },
+        headerTitleStyle: {
+          color: Colors.white,
+          fontFamily: "FiraSans",
+          fontSize: Fonts.f20,
+        },
+        headerTitleAlign: "center",
+        sceneStyle: {
+          backgroundColor: Colors.black,
+        },
+      })}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          title: "My courses",
+        }}
+      />
+    </Drawer>
   );
 }
--- shared/tokens.tsx
+++ shared/tokens.tsx
@@ -1,28 +1,30 @@
 export const Colors = {
-    black: "#16171D",
-    gray: "#AFB2BF",
-    violetDark: "#2E2D3D",
-    primaryHover: "#452481",
-    primary: "#6C38CC",
-    link: "#A97BFF",
-    white: "#FAFAFA",
-    red: "#CC384E"
-}
+  black: "#16171D",
+  blackLight: "#1E1F29",
+  gray: "#AFB2BF",
+  violetDark: "#2E2D3D",
+  primaryHover: "#452481",
+  primary: "#6C38CC",
+  link: "#A97BFF",
+  white: "#FAFAFA",
+  red: "#CC384E",
+};
 
 export const Gaps = {
-    g16: 16,
-    g50: 50
-}
+  g16: 16,
+  g50: 50,
+};
 
 export const Radius = {
-    r10: 10,
-}
+  r10: 10,
+};
 
 export const Fonts = {
-    f16: 16,
-    f18: 18,
-}
+  f16: 16,
+  f18: 18,
+  f20: 20,
+};
 
 export const Padding = {
-    p48: 48,
-}
+  p48: 48,
+};
