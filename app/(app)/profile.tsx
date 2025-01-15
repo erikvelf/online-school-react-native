68643460c0c4d901f1fdf24af39458580c52f980
Erik Velf <velf.erik@protonmail.com>
2025-01-15 22:14:19 +0100

    Started adding a base for selecting an image for the avatar of the profile

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,9 +1,25 @@
 import { View, Text } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { Button } from "../../shared/Button/Button";
+import { useState } from "react";
 
 export default function Profile() {
+  const [image, setImage] = useState(null);
+
+  const pickAvatar = async () => {
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.5,
+    });
+    console.log(result);
+  };
+
   return (
     <View>
       <Text>Profile</Text>
+      <Button text="Select image" onPress={pickAvatar} />
     </View>
   );
 }
