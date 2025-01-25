29255bcfa811618c74526804821f37e3a72a8cce
Erik Velf <velf.erik@protonmail.com>
2025-01-25 23:05:04 +0100

    Added loading course data of a profile

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,14 +1,32 @@
 import { View, Text } from "react-native";
-import { useSetAtom } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import { logoutAtom } from "../../entities/auth/model/auth.state";
-import { Button } from "../../shared/Button/Button";
+import {
+  courseAtom,
+  loadCourseAtom,
+} from "../../entities/course/model/course.state";
+import { useEvent } from "expo";
+import { useEffect } from "react";
 
 export default function MyCoursesPage() {
-  const logout = useSetAtom(logoutAtom);
+  const { isLoading, error, courses } = useAtomValue(courseAtom);
+  const loadCourses = useSetAtom(loadCourseAtom);
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+
   return (
     <View>
       <Text>Index</Text>
-      <Button text="Logout" onPress={logout} />
+      {courses.length > 0 &&
+        courses.map((course) => {
+          return (
+            <View key={course.id}>
+              <Text style={{ color: "white" }}>{course.title}</Text>
+            </View>
+          );
+        })}
     </View>
   );
 }
---
+++ entities/course/model/course.state.ts
@@ -0,0 +1,57 @@
+import axios, { AxiosError } from "axios";
+import { atom } from "jotai";
+import { authAtom } from "../../auth/model/auth.state";
+import { API } from "../api/api";
+import { StudentCourseDescription } from "./course.model";
+
+export const courseAtom = atom<CourseState>({
+  courses: [],
+  isLoading: false,
+  error: null,
+});
+
+export const loadCourseAtom = atom(
+  async (get) => {
+    return get(courseAtom);
+  },
+  async (get, set) => {
+    try {
+      const { accessToken } = await get(authAtom);
+      set(courseAtom, {
+        isLoading: true,
+        courses: [],
+        error: null,
+      });
+      const { data } = await axios.get<StudentCourseDescription[]>(API.my, {
+        params: {
+          studentCourse: "dontMy",
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      console.log(data);
+
+      set(courseAtom, {
+        isLoading: false,
+        courses: data.my,
+        error: null,
+      });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        set(courseAtom, {
+          isLoading: false,
+          courses: [],
+          error: error.response?.data.message,
+        });
+      }
+    }
+  },
+);
+
+export interface CourseState {
+  isLoading: boolean;
+  courses: StudentCourseDescription[];
+  error: null;
+}
