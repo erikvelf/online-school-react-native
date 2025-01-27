2065f77872c98318e26882abdaa1db00964165b4
Erik Velf <velf.erik@protonmail.com>
2025-01-27 19:53:12 +0100

    Added pull to refresh courses using RefreshControl

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,4 +1,9 @@
-import { StyleSheet, View } from "react-native";
+import {
+  ActivityIndicator,
+  RefreshControl,
+  StyleSheet,
+  View,
+} from "react-native";
 import { useAtomValue, useSetAtom } from "jotai";
 import {
   courseAtom,
@@ -9,6 +14,7 @@ import { CourseCard } from "../../entities/course/ui/CourseCard";
 import { FlatList } from "react-native";
 import { StudentCourseDescription } from "../../entities/course/model/course.model";
 import React from "react";
+import { Colors } from "../../shared/tokens";
 
 export default function MyCoursesPage() {
   const { isLoading, error, courses } = useAtomValue(courseAtom);
@@ -29,8 +35,24 @@ export default function MyCoursesPage() {
   return (
     // using FlatList instead of ScrollView because it lazy-loads its elements
     <>
+      {/* Showing activity indicator when courses are loading */}
+      {isLoading && (
+        <ActivityIndicator
+          style={styles.activity}
+          color={Colors.primary}
+          size={"large"}
+        />
+      )}
+      {/* Adding refreshControl to add 'pull to refresh' functionality */}
       {courses.length > 0 && (
         <FlatList
+          refreshControl={
+            <RefreshControl
+              onRefresh={loadCourses}
+              tintColor={Colors.primary}
+              refreshing={isLoading}
+            />
+          }
           data={courses}
           keyExtractor={(item) => item.id.toString()}
           renderItem={renderCourse}
@@ -43,5 +65,8 @@ export default function MyCoursesPage() {
 const styles = StyleSheet.create({
   item: {
     padding: 20,
+  },
+  activity: {
+    marginTop: 40,
   },
 });
